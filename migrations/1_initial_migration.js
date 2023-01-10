5810d6b5a3a3a55bafc825e67c4d748d3081c65a
steevecheng <steevecheng.coin@gmail.com>
2023-01-11 01:43:30 +0800

    change migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const LPStaking = artifacts.require("LPStaking");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  // deployer.deploy(LPStaking);
 };
