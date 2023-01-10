7e0db9c5ac53bb5a357d258e636096dea176c59f
maiermarc <maiermarc75@gmail.com>
2023-01-11 00:39:16 +0800

    create git branch truffle

--- scripts/testnet_swap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# usage:
-#
-#   $ npx hardhat --network LOCALNETWORK --pool-id LOCAL_POOL_ID --target-network DSTNETWORK --dst-pool-id DST_POOLID
-eval "npx hardhat --network ${1} testnetSwap --pool-id ${2} --target-network ${3} --dst-pool-id ${4} --qty ${5}"
