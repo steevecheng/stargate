54d6ea6f10870319d98232cbde6ef38c061545c0
steevecheng <steevecheng.coin@gmail.com>
2023-01-11 13:45:57 +0800

    try to migrate StargateToken.sol

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 const Migrations = artifacts.require("Migrations");
 const LPStaking = artifacts.require("LPStaking");
+const StargateToken = artifacts.require("StargateToken");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
   // deployer.deploy(LPStaking);
+  deployer.deploy(StargateToken, "StargateToken", "STG", "0x8731d54E9D02c286767d56ac03e8037C07e01e98", "1", "1")
 };
